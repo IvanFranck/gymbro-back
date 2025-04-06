3ec28651b2511902deacb5c9e0d4925a8cb3d0a9
NZIMA Ivan <nzimaivan007@gmail.com>
2025-04-06 19:48:30 +0100

    Enhance CustomerServiceModule by adding PrismaService and ConfigService as providers

--- src/customer-service/customer-service.module.ts
+++ src/customer-service/customer-service.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CustomerServiceService } from './customer-service.service';
 import { CustomerServiceController } from './customer-service.controller';
+import { PrismaService } from 'src/prisma.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [CustomerServiceService],
-  controllers: [CustomerServiceController]
+  providers: [CustomerServiceService, PrismaService, ConfigService],
+  controllers: [CustomerServiceController],
 })
 export class CustomerServiceModule {}
